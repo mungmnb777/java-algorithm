a0c7c4e8ca12a59ab7f71e27d917b8a4b861ef4f
myeongbeom <mungmnb777@gmail.com>
2022-03-15 23:33:14 +0900

    백준 4153 직각삼각형

---
+++ code/boj/Main_4153_직각삼각형.java
@@ -0,0 +1,30 @@
+package boj.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_4153_직각삼각형 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+            int[] arr = new int[3];
+
+            for (int i = 0; i < 3; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+
+            Arrays.sort(arr);
+
+            if (arr[0] == 0 && arr[1] == 0 && arr[2] == 0) break;
+
+            if (arr[2] * arr[2] == arr[0] * arr[0] + arr[1] * arr[1]) System.out.println("right");
+            else System.out.println("wrong");
+        }
+    }
+}

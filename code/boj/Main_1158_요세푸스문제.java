20f55363af7bc46cc46b3513ac272c1f22e8c627
myeongbeom <mungmnb777@gmail.com>
2022-03-19 23:58:10 +0900

    백준 1158 요세푸스문제

---
+++ code/boj/Main_1158_요세푸스문제.java
@@ -0,0 +1,36 @@
+package boj.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main_1158_요세푸스문제 {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+
+		Queue<Integer> queue = new LinkedList<>();
+
+		for (int i = 1; i <= N; i++) {
+			queue.offer(i);
+		}
+
+		StringBuilder sb = new StringBuilder().append("<");
+		while (!queue.isEmpty()) {
+			for (int i = 0; i < K - 1; i++) {
+				queue.offer(queue.poll());
+			}
+			sb.append(queue.poll()).append(", ");
+		}
+		sb.setLength(sb.length() - 2);
+		sb.append(">");
+		System.out.println(sb);
+	}
+}

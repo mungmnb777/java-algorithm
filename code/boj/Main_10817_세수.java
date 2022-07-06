fc2ab45bcca9874be117932eb97f74c91880fd27
myeongbeom <mungmnb777@gmail.com>
2022-07-06 23:18:16 +0900

    백준 10817 세 수

---
+++ code/boj/Main_10817_세수.java
@@ -0,0 +1,19 @@
+package boj.bronze;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main_10817_세수 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] nums = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        Arrays.sort(nums);
+
+        System.out.println(nums[1]);
+    }
+}
